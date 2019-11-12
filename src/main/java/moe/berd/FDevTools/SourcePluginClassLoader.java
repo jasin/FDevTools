50a558ad976e5e9bdd7839833d2f23f7fde0e91e
Jasin Colegrove <jasin@coleburt.com>
2019-11-12 12:41:08 +0000

    Updated code with the latest changes to Nukkit Cleaned up and improved readability of code Changes to be committed: 	modified:   src/main/java/moe/berd/FDevTools/SourcePluginClassLoader.java 	modified:   src/main/java/moe/berd/FDevTools/SourcePluginLoader.java

--- src/main/java/moe/berd/FDevTools/SourcePluginClassLoader.java
+++ src/main/java/moe/berd/FDevTools/SourcePluginClassLoader.java
@@ -4,64 +4,54 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public class SourcePluginClassLoader extends URLClassLoader
-{
-	private String pluginPath="";
-	private SourcePluginLoader loader=null;
-	private Map<String,Class> classes=new HashMap<>();
+public class SourcePluginClassLoader extends URLClassLoader {
+
+	private String pluginPath;
+	private SourcePluginLoader loader;
+	private Map<String,Class> classes = new HashMap<>();
 	
-	public SourcePluginClassLoader(SourcePluginLoader loader,ClassLoader parent,File file,String pluginPath) throws MalformedURLException
-	{
-		super(new URL[]
-		{
-			file.toURI().toURL()
-		},parent);
+	public SourcePluginClassLoader(SourcePluginLoader loader, ClassLoader parent, File file, String pluginPath) throws MalformedURLException {
+		
+        super(new URL[]{file.toURI().toURL()},parent);
 		this.loader=loader;
 		this.pluginPath=pluginPath.replace("\\","/")+"/";
 	}
-	
-	protected Class findClass(String name) throws ClassNotFoundException
-	{
-		return this.findClass(name,true);
-	}
-	
-	protected Class findClass(String name,boolean checkGlobal) throws ClassNotFoundException
-	{
-		if(!name.startsWith("cn.nukkit.") && !name.startsWith("net.minecraft."))
-		{
-			Class result=this.classes.get(name);
-			if(result==null)
-			{
-				if(checkGlobal)
-				{
-					result=this.loader.getClassByName(name);
-				}
-				if(result==null)
-				{
-					result=super.findClass(name);
-					if(result!=null)
-					{
-						this.loader.setClass(name,result);
-					}
-				}
-				this.classes.put(name,result);
-			}
-			return result;
-		}
-		else
-		{
-			throw new ClassNotFoundException(name);
-		}
-	}
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        return this.findClass(name, true);
+    }
+
+    protected Class<?> findClass(String name, boolean checkGlobal) throws ClassNotFoundException {
+        if(name.startsWith("cn.nukkit.") || name.startsWith("net.minecraft.")) {
+            throw new ClassNotFoundException(name);
+        }
+
+        Class<?> result = classes.get(name);
+
+        if(result == null) {
+            if(checkGlobal) {
+                result = loader.getClassByName(name);
+            }
+
+            if(result == null) {
+                result = super.findClass(name);
+                if(result != null) {
+                    loader.setClass(name, result);
+                }
+            }
+
+            classes.put(name, result);
+        }
+
+        return result;
+    }
 	
 	@Override
-	public URL findResource(String name)
-	{
-		try
-		{
+	public URL findResource(String name) {
+		try {
 			File res=new File(this.pluginPath+"src/main/resources/"+name);
-			if(res.exists())
-			{
+			if(res.exists()) {
 				return res.toURI().toURL();
 			}
 		}
@@ -70,10 +60,5 @@ public class SourcePluginClassLoader extends URLClassLoader
 			
 		}
 		return null;
-	}
-	
-	public Set getClasses()
-	{
-		return this.classes.keySet();
 	}
 }
--- src/main/java/moe/berd/FDevTools/SourcePluginLoader.java
+++ src/main/java/moe/berd/FDevTools/SourcePluginLoader.java
@@ -27,81 +27,69 @@ public class SourcePluginLoader implements PluginLoader
 	private Map<PluginBase,File> pluginPath=new HashMap<>();
 	private Map<String,SourcePluginClassLoader> classLoaders=new HashMap<>();
 	
-	public SourcePluginLoader(Server server)
-	{
+	public SourcePluginLoader(Server server) {
 		this.server=server;
 		this.plugin=Main.getInstance();
 		this.compiler=this.plugin.getCompiler();
 		this.filemanager=this.compiler.getStandardFileManager(null,null,null);
 	}
 	
-	public Plugin loadPlugin(File dir) throws Exception
-	{
-		PluginDescription description=this.getPluginDescription(dir);
-		if(description!=null)
-		{
-			if(this.server.getPluginManager().getPlugin(description.getName())!=null)
-			{
-				this.plugin.getLogger().warning("Can't load source plugin \""+description.getName()+"\",plugin exists.");
-				return null;
+    @Override
+	public Plugin loadPlugin(File dir) throws Exception {
+		PluginDescription description = this.getPluginDescription(dir);
+		if(description != null) {
+			this.plugin.getLogger().info(TextFormat.AQUA+"Loading source plugin \""+description.getName()+"\"");
+			File class_file = new File(dir.getAbsolutePath()+"/src_compile");
+			this.compilePlugin(dir,class_file);
+			SourcePluginClassLoader classLoader = new SourcePluginClassLoader(this, this.getClass().getClassLoader(), class_file, dir.getAbsolutePath());
+			this.classLoaders.put(description.getName(),classLoader);
+			File dataFolder = new File(dir.getParentFile(), description.getName());
+			if(dataFolder.exists() && !dataFolder.isDirectory()) {
+				throw new IllegalStateException("Compile target folder \""+dataFolder.toString()+"\" for "+description.getName()+" exists and is not a directory");
 			}
-			else
-			{
-				this.plugin.getLogger().info(TextFormat.AQUA+"Loading source plugin \""+description.getName()+"\"");
-				File class_file=new File(dir.getAbsolutePath()+"/src_compile");
-				this.compilePlugin(dir,class_file);
-				SourcePluginClassLoader classLoader=new SourcePluginClassLoader(this,this.getClass().getClassLoader(),class_file,dir.getAbsolutePath());
-				this.classLoaders.put(description.getName(),classLoader);
-				File dataFolder=new File(dir.getParentFile(),description.getName());
-				if(dataFolder.exists() && !dataFolder.isDirectory())
-				{
-					throw new IllegalStateException("Compile target folder \""+dataFolder.toString()+"\" for "+description.getName()+" exists and is not a directory");
-				}
-				else
-				{
-					try
-					{
-						PluginBase plugin=classLoader.loadClass(description.getMain()).asSubclass(PluginBase.class).newInstance();
-						this.initPlugin(plugin,description,dataFolder,dir);
-						this.pluginPath.put(plugin,dir);
-						return plugin;
-					}
-					catch(ClassCastException e)
-					{
-						throw new Exception("Main class \""+description.getMain()+"\" does not extend PluginBase");
-					}
-					catch(ClassNotFoundException e)
-					{
-						throw new Exception("Couldn't load plugin "+description.getName()+": main class not found");
-					}
-				}
-			}
-		}
-		else
-		{
-			return null;
-		}
+
+            PluginBase plugin;
+			try {
+			    Class sourcePluginClass = classLoader.loadClass(description.getMain());
+
+                if(!PluginBase.class.isAssignableFrom(sourcePluginClass)) {
+                    throw new PluginException("Main class '" + description.getMain() + "' does not extend PluginBase");
+                }
+
+                try {
+                    Class<PluginBase> pluginClass = (Class<PluginBase>) sourcePluginClass.asSubclass(PluginBase.class);
+                    plugin = pluginClass.newInstance();
+                    
+                    this.initPlugin(plugin, description, dataFolder, dir);
+					this.pluginPath.put(plugin, dir);
+					return plugin;
+				} catch(ClassCastException e) {
+					throw new PluginException("Main class \""+description.getMain()+"\" does not extend PluginBase");
+				} catch(InstantiationException | IllegalAccessException e) {
+					Server.getInstance().getLogger().logException(e);
+                }
+		    } catch(ClassNotFoundException e) {
+                throw new PluginException("Couldn't load plugin " + description.getName() + ": main class not found");
+            }
+        }
+		
+        return null;
 	}
 	
-	public PluginDescription getPluginDescription(File dir)
-	{
-		try
-		{
+    @Override
+	public PluginDescription getPluginDescription(File dir) {
+		try {
 			File yml=new File(dir.getAbsolutePath() + "/src/main/resources", "plugin.yml");
-			return dir.isDirectory() && yml.isFile()?new PluginDescription(Utils.readFile(yml)):null;
-		} catch (Exception e)
-		{
+			return dir.isDirectory() && yml.isFile() ? new PluginDescription(Utils.readFile(yml)) : null;
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 	}
 	
-	public Pattern[] getPluginFilters()
-	{
-		return new Pattern[]
-		{
-			Pattern.compile(".+")
-		};
+    @Override
+	public Pattern[] getPluginFilters()	{
+		return new Pattern[]{Pattern.compile(".+")};
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -133,88 +121,72 @@ public class SourcePluginLoader implements PluginLoader
 		}
 	}
 	
-   public File getPluginPath(Plugin p)
-   {
-      return this.pluginPath.getOrDefault(p,null);
-   }
+    public File getPluginPath(Plugin p) {
+        return this.pluginPath.getOrDefault(p, null);
+    }
    
-	// Nukkit's code
 	
-	public PluginDescription getPluginDescription(String filename)
-	{
+    public PluginDescription getPluginDescription(String filename) {
 		return this.getPluginDescription(new File(filename));
 	}
 	
-	public Plugin loadPlugin(String filename) throws Exception
-	{
+	public Plugin loadPlugin(String filename) throws Exception {
 		return this.loadPlugin(new File(filename));
 	}
 	
-	private void initPlugin(PluginBase plugin,PluginDescription description,File dataFolder,File file)
-	{
-		plugin.init(this,this.server,description,dataFolder,file);
+	private void initPlugin(PluginBase plugin, PluginDescription description, File dataFolder, File file) {
+		plugin.init(this, this.server, description, dataFolder, file);
 		plugin.onLoad();
 	}
 	
-	public void enablePlugin(Plugin plugin)
-	{
-		if(plugin instanceof PluginBase && !plugin.isEnabled())
-		{
-			this.plugin.getLogger().info(this.server.getLanguage().translateString("nukkit.plugin.enable",plugin.getDescription().getFullName()));
-			((PluginBase)plugin).setEnabled(true);
+    @Override
+	public void enablePlugin(Plugin plugin) {
+		if(plugin instanceof PluginBase && !plugin.isEnabled()) {
+			this.server.getLogger().info(this.server.getLanguage().translateString("nukkit.plugin.enable", plugin.getDescription().getFullName()));
+			((PluginBase) plugin).setEnabled(true);
 			this.server.getPluginManager().callEvent(new PluginEnableEvent(plugin));
 		}
 	}
 	
-	public void disablePlugin(Plugin plugin)
-	{
-		if(plugin instanceof PluginBase && plugin.isEnabled())
-		{
-			this.plugin.getLogger().info(this.server.getLanguage().translateString("nukkit.plugin.disable",plugin.getDescription().getFullName()));
+    @Override
+	public void disablePlugin(Plugin plugin) {
+		if(plugin instanceof PluginBase && plugin.isEnabled()) {
+			this.server.getLogger().info(this.server.getLanguage().translateString("nukkit.plugin.disable",plugin.getDescription().getFullName()));
+            this.server.getServiceManager().cancel(plugin);
 			this.server.getPluginManager().callEvent(new PluginDisableEvent(plugin));
-			((PluginBase)plugin).setEnabled(false);
+			((PluginBase) plugin).setEnabled(false);
 		}
 	}
 	
-	public Class<?> getClassByName(String name)
-	{
+	public Class<?> getClassByName(String name)	{
 		Class<?> cachedClass=classes.get(name);
-		if(cachedClass!=null)
-		{
+
+		if(cachedClass != null) {
 			return cachedClass;
-		}
-		else
-		{
-			for(SourcePluginClassLoader loader:this.classLoaders.values())
-			{
-                try
-				{
+		} else {
+			for(SourcePluginClassLoader loader : this.classLoaders.values()) {
+                
+                try	{
                     cachedClass=loader.findClass(name,false);
+                } catch(ClassNotFoundException e) {
+                    //this.plugin.getLogger().error(e.toString());
                 }
-				catch(ClassNotFoundException e)
-				{
-                    this.plugin.getLogger().error(e.toString());
-                }
-                if(cachedClass!=null)
-				{
+
+                if(cachedClass != null) {
                     return cachedClass;
                 }
             }
-			return null;
+		}
+        return null;
+	}
+	
+	public void setClass(String name, Class<?> clazz) {
+		if(!this.classes.containsKey(name)) {
+			this.classes.put(name, clazz);
 		}
 	}
 	
-	public void setClass(String name,Class<?> clazz)
-	{
-		if(!this.classes.containsKey(name))
-		{
-			this.classes.put(name,clazz);
-		}
-
-	}
-	
-	private void removeClass(String name)
-	{
+	private void removeClass(String name) {
 		this.classes.remove(name);
 	}
 }
